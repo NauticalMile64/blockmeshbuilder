96ecb6e143f00dc01d30a4b66f9466aad55af94a
Nolan James Dyck <nolan.dyck20@gmail.com>
2018-07-15 20:43:29 -0400

    Update setup
    spaces to tabs
    modify author info

--- setup.py
+++ setup.py
@@ -6,23 +6,22 @@ vars = {}
 exec(open(ver_file).read(), vars)
 
 setup(
-    name='ofblockmeshdicthelper',
-    version=vars['__version__'],
-    description='Helper utilities for OpenFOAM blockMeshDict generation.',
-    long_description=open('README.rst', 'rt').read(),
-    author='Takaaki AOKI',
-    author_email='aoki.takaaki@gmail.com',
-    url='https://github.com/takaakiaoki/ofblockmeshdicthelper',
-    packages=find_packages(),
-    package_dir={'ofblockmeshdicthelper': 'ofblockmeshdicthelper'},
-    install_requires=['six'],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Physics"])
+	name='ofblockmeshdicthelper',
+	version=vars['__version__'],
+	description='Helper utilities for OpenFOAM blockMeshDict generation.',
+	long_description=open('README.rst', 'rt').read(),
+	author='Nolan Dyck',
+	url='https://github.com/NauticalMile64/ofblockmeshdicthelper',
+	packages=find_packages(),
+	package_dir={'ofblockmeshdicthelper': 'ofblockmeshdicthelper'},
+	install_requires=['six'],
+	classifiers=[
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 2.7",
+		"License :: OSI Approved :: MIT License",
+		"Development Status :: 4 - Beta",
+		"Environment :: Other Environment",
+		"Intended Audience :: Science/Research",
+		"Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: Physics"])
