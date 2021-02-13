a8bca47659918fa7857a513cad898948f5309852
NauticalMile64 <nolan.dyck20@gmail.com>
2021-02-13 11:27:02 -0500

    Add airfoil example
    - Produces a mesh for a NACA 4-digit airfoil given user input airfoil and mesh specifications

---
+++ tests/airfoil.py
@@ -0,0 +1,153 @@
+import numpy as np
+from airfoil_points import NACA4
+from ofblockmeshdicthelper import BlockMeshDict, CartBlockStruct, Point, \
+	BSplineCurvedEdge, PolyLineCurvedEdge, SimpleGradingElement, MultiGradingElement, get_grading_info, Boundary
+import shapely.geometry as shp
+
+# Airfoil shape and dimensions
+close_trailing_edge = True
+airfoil = NACA4((4, 4, 24), close_trailing_edge=close_trailing_edge)
+chord_length = 1.0
+num_chords_upstream = 1.5
+num_chords_downstream = 2.0
+num_chords_normal = 1.5
+num_chords_bl_thickness = 0.05
+num_chords_span = 0.1
+
+# Node counts
+nx_upstream = np.round(60*num_chords_upstream)
+nx_chord = 100
+nx_downstream = np.round(75*num_chords_downstream)
+ny_wall_normal = np.round(75*num_chords_downstream)
+nz_span = np.round(10*num_chords_span)
+n_bl = np.round(350*num_chords_bl_thickness)
+n_nose = 22
+
+# Spline Resolution
+spine_points = 100
+spline_res = 1
+
+# Create the blockMeshDict object
+block_mesh_dict = BlockMeshDict()
+block_mesh_dict.set_metric('mm')
+
+# Create boundary layer struct
+bl_ys_positive = np.array([0.05, 1.0, num_chords_downstream + 1])
+bl_ys = np.concatenate((-bl_ys_positive[::-1], bl_ys_positive))
+bl_xs = np.array([0., num_chords_bl_thickness]) - 0.5
+zs = np.array([-0.5, 0.5]) * num_chords_span
+
+bl_ndy_positive = [nx_chord, nx_downstream]
+bl_ndy = np.array(bl_ndy_positive[::-1] + [n_nose] + bl_ndy_positive + [0])
+bl_ndx = np.array([n_bl, 0])
+ndz = np.array([nz_span, 0])
+
+bl_struct = CartBlockStruct(bl_xs, bl_ys, zs, bl_ndx, bl_ndy, ndz, zone='boundary_layer')
+
+# Get airfoil surface points using cosine spacing: http://airfoiltools.com/airfoil/naca4digit
+x = (1 - np.cos(np.linspace(0, np.pi, spine_points))) / 2
+xu, yu, xl, yl = airfoil.calculate_surface_points(x)
+u_pts = np.dstack((xu, yu))
+l_pts = np.dstack((xl, yl))
+s_pts = np.hstack((l_pts[:, ::-1], u_pts))[0]
+
+# Get the boundary layer outer profile
+af_line_string = shp.LineString(s_pts)
+bl_pts = np.array(af_line_string.parallel_offset(num_chords_bl_thickness, 'left'))
+
+# Need to separate the lists of points into y, x arrays for gradient function
+with np.errstate(divide='ignore', invalid='ignore'):
+	sf_slope = np.nan_to_num(np.gradient(*s_pts[:, ::-1].T), copy=False, nan=np.inf)
+	bl_slope = np.nan_to_num(np.gradient(*bl_pts[:, ::-1].T), copy=False, nan=np.inf)
+
+# Find projection co-ordinate indices
+leading_edge_slope = np.gradient(airfoil.calculate_camber_line([0., 0.001]), [0., 0.001])[0]
+p_slope = np.tan(np.pi / 4 + np.arctan(leading_edge_slope))
+m_slope = -np.tan(np.pi / 4 - np.arctan(leading_edge_slope))
+
+u_nose_index = (np.abs(sf_slope - p_slope)).argmin()
+l_nose_index = (np.abs(sf_slope - m_slope)).argmin()
+ub_nose_index = (np.abs(bl_slope - p_slope)).argmin()
+lb_nose_index = (np.abs(bl_slope - m_slope)).argmin()
+
+sb_idx = [0, l_nose_index, u_nose_index, -1]
+bb_idx = [0, lb_nose_index, ub_nose_index, -1]
+
+bl_struct['vertices'][-1, 1:5, :, :2] = s_pts[sb_idx][:, np.newaxis]
+bl_struct['vertices'][0, 1:5, :, :2] = bl_pts[bb_idx][:, np.newaxis]
+
+bl_edges = bl_struct['edges']
+bl_b_vts = bl_struct['baked_vertices']
+
+for j in range(1, 4):
+	for k in range(zs.size):
+		pts = [Point(np.append(b_pt, zs[k])) for b_pt in bl_pts[bb_idx[j - 1] + 1:bb_idx[j]:spline_res]]
+		bl_edges[0, j, k, 1] = PolyLineCurvedEdge(bl_b_vts[0, j:j + 2, k], pts)
+
+		pts = [Point(np.append(s_pt, zs[k])) for s_pt in s_pts[sb_idx[j - 1] + 1:sb_idx[j]:spline_res]]
+		bl_edges[-1, j, k, 1] = PolyLineCurvedEdge(bl_b_vts[-1, j:j + 2, k], pts)
+
+# Align downstream boundary layer block structure
+bl_struct['vertices'][:, [0, -1], :, 0] = 1 + num_chords_downstream
+bl_struct['vertices'][0, 0, :, 1] = -num_chords_bl_thickness
+bl_struct['vertices'][-1, [0, -1], :, 1] = bl_struct['vertices'][-1, [1, -2], :, 1]
+bl_struct['vertices'][0, -1, :, 1] = num_chords_bl_thickness
+
+te_struct = None
+if close_trailing_edge:
+	bl_struct['baked_vertices'][-1, [-1, -2]] = bl_struct['baked_vertices'][-1, [0, 1]]
+else:
+	te_struct = CartBlockStruct([0, 1], [0, 1], zs, [1, 0], [nx_downstream, 0], ndz)
+	te_struct['baked_vertices'][0] = bl_struct['baked_vertices'][-1, :2]
+	te_struct['baked_vertices'][-1] = bl_struct['baked_vertices'][-1, -2:][::-1]
+	te_struct.write(block_mesh_dict)
+
+# Create the far-field structure
+ff_xs = np.array([-num_chords_upstream, 0., 1., 1 + num_chords_downstream])
+ff_ys_positive = np.array([num_chords_bl_thickness, num_chords_normal])
+ff_ys = np.concatenate((-ff_ys_positive[::-1], ff_ys_positive))
+
+ff_ndx = np.array([nx_upstream, nx_chord, nx_downstream, 0])
+ff_ndy = np.array([ny_wall_normal, n_nose, ny_wall_normal, 0])
+ff_struct = CartBlockStruct(ff_xs, ff_ys, zs, ff_ndx, ff_ndy, ndz, zone='far_field')
+
+# Cut out the center of the far-field struct for the boundary layer struct to fit
+ff_struct['block_mask'][1:, 1, :] = True
+
+# Mate far-field structure to boundary layer structure
+ff_struct['baked_vertices'][1:, 1] = bl_struct['baked_vertices'][0, 2::-1]
+ff_struct['baked_vertices'][1:, 2] = bl_struct['baked_vertices'][0, 3:]
+
+# Mesh tuning
+bl_struct['grading'][0, ..., 0] = SimpleGradingElement(1./2)
+
+len_pcts = np.array([0.8, 0.2])
+dens = np.array([1., 1., 3.])
+ff_struct['grading'][0, ..., 0] = MultiGradingElement(*get_grading_info(len_pcts, dens))
+ff_struct['grading'][:, 0, :, 1] = MultiGradingElement(*get_grading_info(len_pcts, dens))
+ff_struct['grading'][:, 2, :, 1] = MultiGradingElement(*get_grading_info(len_pcts[::-1], dens[::-1]))
+
+downstream_x_grading = MultiGradingElement(*get_grading_info(len_pcts[::-1], dens[::-1]))
+# ff_struct['grading'][-2, ..., 0] = downstream_x_grading
+# bl_struct['grading'][0, 0, :, 1] = downstream_x_grading
+
+# Write the blocks to the blockMeshDict
+bl_struct.write(block_mesh_dict)
+ff_struct.write(block_mesh_dict)
+# if te_struct:
+# 	te_struct.write((block_mesh_dict))
+
+# Write to file
+with open(r'OF_case/system/blockMeshDict', 'w') as infile:
+	infile.write(block_mesh_dict.format())
+
+import matplotlib.pyplot as plt
+
+plt.plot(*s_pts.T)
+plt.plot(*bl_pts.T)
+plt.scatter(*s_pts[[u_nose_index, l_nose_index]].T)
+plt.scatter(*bl_pts[[ub_nose_index, lb_nose_index]].T)
+ax = plt.gca()
+ax.set_aspect('equal')
+
+# plt.show()
---
+++ tests/airfoil_points.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+class NACA4:
+
+	def __init__(self, foil, close_trailing_edge=False):
+		m = float(foil[0]) / 100.  # max camber
+		p = float(foil[1]) / 10.   # chord-wise position of max camber
+		t = float(foil[2]) / 100.  # thickness
+		self.foil = (m, p, t)
+		if close_trailing_edge:
+			self.c4 = -0.1036
+		else:
+			self.c4 = -0.1015
+
+	def calculate_camber_line(self, x):
+		x = np.asarray(x)
+		m, p = self.foil[0], self.foil[1]
+		yc = np.asarray(2 * p * x - x ** 2)
+		sel_a = x <= p
+		yc[sel_a] *= m / p ** 2
+		sel_b = np.logical_not(sel_a)
+		yc[sel_b] = m / (1 - p) ** 2 * (1 - 2 * p + yc[sel_b])
+		return yc
+
+	def calculate_surface_points(self, x):
+		x = np.asarray(x)
+		yt = 5 * self.foil[2] * (0.2969 * np.sqrt(x) - 0.126 * x - 0.3516 * x ** 2 + 0.2843 * x ** 3 + self.c4 * x ** 4)
+
+		# If there is any camber
+		m = self.foil[0]
+		if m:
+			yc = self.calculate_camber_line(x)
+
+			p = self.foil[1]
+			dydx = np.asarray(2 * m * (p - x) / p ** 2)
+			dydx[x > p] *= p ** 2 / (1 - p) ** 2
+			theta = np.arctan(dydx)
+
+			yt_sin = yt * np.sin(theta)
+			yt_cos = yt * np.cos(theta)
+			xu = x - yt_sin
+			yu = yc + yt_cos
+			xl = x + yt_sin
+			yl = yc - yt_cos
+			return xu, yu, xl, yl
+
+		else:
+			return x, yt, x, -yt
+
+	def calculate_leading_edge_radius(self):
+		return 1.1019*(float(self.foil[2])/100.)**2
+
+
+if __name__ == "__main__":
+
+	NACA_code = (2, 4, 12)
+	x_points = (1 - np.cos(np.linspace(0, np.pi, 100))) / 2
+	a2412 = NACA4(NACA_code, close_trailing_edge=False)
+	xu1, yu1, xl1, yl1 = a2412.calculate_surface_points(x_points)
+	xu2, yu2, xl2, yl2 = NACA4(NACA_code, close_trailing_edge=True).calculate_surface_points(x_points)
+
+	leading_edge_radius = 1.1019*(12./100)**2
+	leading_edge_y = a2412.calculate_camber_line(leading_edge_radius)
+	print(a2412.calculate_surface_points(0.05))
+
+	import matplotlib.pyplot as plt
+
+	plt.plot(xu1, yu1)
+	plt.scatter(xl1, yl1)
+	circle = plt.Circle((leading_edge_radius, leading_edge_y), leading_edge_radius, color='blue')
+	plt.plot(xu2, yu2, color='orange')
+	plt.scatter(xl2, yl2, color='orange')
+	ax = plt.gca()
+	ax.add_patch(circle)
+	ax.set_aspect('equal')
+	plt.show()
