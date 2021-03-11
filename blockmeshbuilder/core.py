e5469381558cd3e9820b621951ca0068cec41549
NauticalMile64 <nolan.dyck20@gmail.com>
2021-03-11 11:02:22 -0500

    Bugfix: cast arrays to floating type to avoid integer rounding errors in conversion functions

--- blockmeshbuilder/core.py
+++ blockmeshbuilder/core.py
@@ -19,7 +19,7 @@ def cart_to_cart(crds):
 
 
 def cyl_to_cart(crds):
-	crds = np.asarray(crds)
+	crds = np.asarray(crds, dtype=np.floating)
 	ncrds = crds.copy()
 	ncrds[..., 0] = np.multiply(crds[..., 0], np.cos(crds[..., 1]))
 	ncrds[..., 1] = np.multiply(crds[..., 0], np.sin(crds[..., 1]))
@@ -27,7 +27,7 @@ def cyl_to_cart(crds):
 
 
 def cart_to_cyl(crds):
-	crds = np.asarray(crds)
+	crds = np.asarray(crds, dtype=np.floating)
 	ncrds = crds.copy()
 	ncrds[..., 0] = np.hypot(crds[..., 0], crds[..., 1])
 	ncrds[..., 1] = np.arctan2(crds[..., 1], crds[..., 0])
