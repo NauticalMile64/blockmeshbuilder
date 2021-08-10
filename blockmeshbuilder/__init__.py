adaaee23d6dfe4424bf5b389bfb6e623bac79b33
NauticalMile64 <nolan.dyck20@gmail.com>
2021-08-10 12:12:13 -0400

    Limit classes and objects exposed to the user to clean up library interface

--- blockmeshbuilder/__init__.py
+++ blockmeshbuilder/__init__.py
@@ -1,8 +1,9 @@
 from .blockstructures import *
-from .blockelements import *
+from .blockelements import cart_to_cart, cyl_to_cart, cart_to_cyl, Point, ArcEdge, SplineCurvedEdge, \
+	PolyLineCurvedEdge, BSplineCurvedEdge, ProjectionEdge
 from .zone_tags import ZoneTag
 from .boundary_tags import BoundaryTag
-from .grading import *
+from .grading import SimpleGradingElement, MultiGradingElement, get_grading_info, uniformGradingElement
 from .geometry import PlanePointAndNormal, PlaneEmbeddedPoints, PlaneEquation, Sphere, Cylinder, Cone
 from .blockmeshdict import BlockMeshDict
 from .utilities import *
