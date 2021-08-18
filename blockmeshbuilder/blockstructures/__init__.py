f21edde32964f1c4df74bd4f4741f4e35c174816
NauticalMile64 <nolan.dyck20@gmail.com>
2021-08-18 10:58:28 -0400

    Bugfix: add boundary_structure file to __init__.py

--- blockmeshbuilder/blockstructures/__init__.py
+++ blockmeshbuilder/blockstructures/__init__.py
@@ -1,3 +1,4 @@
 from .cartesian import CartBlockStruct
 from .tube import TubeBlockStruct
 from .cylinder import CylBlockStructContainer
+from .boundary_structure import create_boundary_block_structure
