2eab7849ed306e8802bd4d98cd274fb1276a94ae
NauticalMile64 <nolan.dyck20@gmail.com>
2022-06-21 13:55:07 -0400

    Expose co-ordinate conversion pairs to module top level

--- blockmeshbuilder/__init__.py
+++ blockmeshbuilder/__init__.py
@@ -1,6 +1,6 @@
 from .blockstructures import *
-from .blockelements import cart_to_cart, cyl_to_cart, cart_to_cyl, Point, ArcEdge, SplineCurvedEdge, \
-	PolyLineCurvedEdge, BSplineCurvedEdge, ProjectionEdge
+from .blockelements import cart_to_cart, cyl_to_cart, cart_to_cyl, cart_conv_pair, cyl_conv_pair, Point, ArcEdge, \
+	SplineCurvedEdge, PolyLineCurvedEdge, BSplineCurvedEdge, ProjectionEdge
 from .zone_tags import ZoneTag
 from .boundary_tags import BoundaryTag
 from .grading import SimpleGradingElement, MultiGradingElement, get_grading_info, uniformGradingElement
